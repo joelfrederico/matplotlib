225543c593a686ec026ae748a3c7d54b28a666a0
Joel Frederico <458871+joelfrederico@users.noreply.github.com>
2019-08-01 11:30:18 -0700

    Fix tests on non-Darwin platforms

--- lib/matplotlib/tests/test_backend_macos_common.py
+++ lib/matplotlib/tests/test_backend_macos_common.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 import matplotlib.backends.backend_macos_common as bmc
 from matplotlib import cbook
@@ -8,24 +9,29 @@ class NoString:
         pass
 
 
-def test_set_mac_icon():
-    with pytest.raises(TypeError):
-        bmc.set_mac_icon()
+if sys.platform == "darwin":
+    def test_set_mac_icon():
+        with pytest.raises(TypeError):
+            bmc.set_mac_icon()
 
-    with pytest.raises(RuntimeError):
-        bmc.set_mac_icon(1)
+        with pytest.raises(RuntimeError):
+            bmc.set_mac_icon(1)
 
-    with pytest.raises(RuntimeError):
-        bmc.set_mac_icon([1, 2, 'a'])
+        with pytest.raises(RuntimeError):
+            bmc.set_mac_icon([1, 2, 'a'])
 
-    with pytest.raises(RuntimeError):
-        bmc.set_mac_icon(None)
+        with pytest.raises(RuntimeError):
+            bmc.set_mac_icon(None)
 
-    with pytest.raises(TypeError):
-        bmc.set_mac_icon(NoString())
+        with pytest.raises(TypeError):
+            bmc.set_mac_icon(NoString())
 
-    with pytest.raises(RuntimeError):
-        bmc.set_mac_icon("invalid path")
+        with pytest.raises(RuntimeError):
+            bmc.set_mac_icon("invalid path")
 
-    icon = cbook._get_data_path("images/matplotlib.pdf")
-    bmc.set_mac_icon(icon)
+        icon = cbook._get_data_path("images/matplotlib.pdf")
+        bmc.set_mac_icon(icon)
+else:
+    def test_set_mac_icon():
+        icon = cbook._get_data_path("images/matplotlib.pdf")
+        bmc.set_mac_icon(icon)
